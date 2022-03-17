e1052f2283c93256050c088cc762d3619c800896
Josen190 <92809532+Josen190@users.noreply.github.com>
2022-03-17 17:25:46 +0400

    +

---
+++ laba1/Program.cs
@@ -0,0 +1,30 @@
+﻿using RIAT.serializer;
+using RIAT.unit;
+
+namespace serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input input = Input.CreteRandom();
+            //Output output = Output.createFrom(input);
+
+            //Console.WriteLine($"Несериализованный объект {input}");
+
+            //var serialization = new Serializer();
+            //var inputDiskSerializedJOSN = serialization.serializationJosn(output);
+            //var inputDiskSerializedXml = serialization.serializationXml(output);
+
+            //Console.WriteLine($"Cериализованный объект {inputDiskSerializedJOSN}");
+            //Console.WriteLine($"Cериализованный объект XML {inputDiskSerializedXml}");
+
+            //var outputJosn = serialization.deserializeJson<Output>(inputDiskSerializedJOSN);
+            //var outputXml = serialization.deserializeXml<Output>(inputDiskSerializedXml);
+
+            //outputJosn.Write();
+            //outputXml.Write();
+        }
+    }
+}
+
