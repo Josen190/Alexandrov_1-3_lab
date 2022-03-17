fc42d43390f4575abae2bd69c0b671f4c194e504
Josen190 <92809532+Josen190@users.noreply.github.com>
2022-03-17 17:30:40 +0400

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿using RIAT.Client;
+using RIAT.Server;
+
+namespace serializer
+{
+    class Program
+    {
+        static string url = "127.0.0.1";
+        static int port = 7000;
+        static void Main(string[] args)
+        {
+            RunServer runServer = new RunServer();
+            //Server server = new Server();
+            //Console.ReadLine();
+            Client client = new Client(url, port);
+
+            client.Ping();
+
+            client.GetInputData();
+
+            client.PostAnswer();
+
+            runServer.server.input.Write();
+            Console.ReadLine();
+
+        }
+    }
+}
+
